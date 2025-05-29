4c6fccf63abb850043ea6bb00f76d5c59b621984
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-05-29 14:04:20 +0700

    MARP-2336 Reformat code

--- azure-servicebus-connector/src/com/axonivy/connector/azure/servicebus/AzureServiceBusReceiverSupplier.java
+++ azure-servicebus-connector/src/com/axonivy/connector/azure/servicebus/AzureServiceBusReceiverSupplier.java
@@ -3,8 +3,8 @@ package com.axonivy.connector.azure.servicebus;
 import com.azure.messaging.servicebus.ServiceBusReceiverClient;
 
 /**
- * Implement this interface to supply your own receiver if you need more
- * configuration possibilities than the provided supplier.
+ * Implement this interface to supply your own receiver if you need more configuration possibilities than the provided
+ * supplier.
  */
 public interface AzureServiceBusReceiverSupplier {
 	/**
